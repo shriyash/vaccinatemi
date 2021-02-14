928d5bd248bd6b75dd3335f81ba1542503238476
Shriyash Jalukar <shriyash1111@gmail.com>
2021-02-14 13:07:27 -0500

    work in progress: adding placeholder content

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            <h1> Home Component </h1>
+            <h2> Timeline </h2>
+            <p> Michigan is currently vaccinating those in <a href="https://www.michigan.gov/documents/coronavirus/1.29.21_Michigan_Vaccine_Strategy_FINAL_714811_7.pdf">Phase 1A, 1B and parts of 1C </a> </p>
             
         </div>
     )
