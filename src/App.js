dee62781f1857e65944991273ad0b59337d6e233
Shriyash Jalukar <shriyash1111@gmail.com>
2021-02-14 10:56:39 -0500

    Adding react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import "./App.css";
+import Home from "./Components/Home";
+import About from "./Components/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <div className="container">
+          <p> Let's add routing! </p>
+
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+        </div>
+      </Switch>
+    </Router>
   );
 }
 
